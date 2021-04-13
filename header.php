e508281df5f69433175aafe2a30d28343c951dfb
Yan <souetre.yan@gmail.com>
2021-04-13 18:31:25 +0200

    front

---
+++ header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="css/header.css">
+
+<h1>TODO+</h1>
+
+<ul class="nav">
+    <li><a id="#" href="#"><input type="button" value="Ajouter"></a></li>
+    <li><a id="#" href="#"><input type="button" value="Modifier"></a></li>
+    <li><a id="#" href="#"><input type="button" value="Supprimer"></a></li>
+</ul>
+
+<ul class="profil">
+    <li><a id="#" href="#">Mon profil</a></li>
+    <li><a id="#" href="#">Se connecter</a></li>
+</ul>
