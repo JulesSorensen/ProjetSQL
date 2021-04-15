40df4b856a7d94c5eb2da44af12f34bcd5471baf
Nekewo <julessorens@gmail.com>
2021-04-15 17:21:30 +0200

    image

--- moncompte.php
+++ moncompte.php
@@ -3,9 +3,10 @@
     $ORes = $Bdd->query("SELECT * FROM users WHERE id = '$_SESSION[id]'");
     if ($users = $ORes->fetch()){ 
         ?>
-        <form action="" method="POST">
-            <div id='container'>
-            <div class='signup'>
+        
+        <div id='container'>
+            <form action="" method="POST">
+                <div class='signup'>
                     <input type='text' placeholder='<?php echo $users->prenom; ?>'  name="prenom"/>
                     <?php
                     if(empty($_POST["prenom"]) == FALSE) {
@@ -24,14 +25,67 @@
                     }?>
                     <input type='text' placeholder='<?php echo $users->email; ?>'  name="email"/>
                     <input type='submit' placeholder='SUBMIT' name="bouton"/>
-            </div>
+                </div>
+            </form>
+            <form action="" method="POST" enctype="multipart/form-data">
                 <div class='whysign'>
                     <h1>PHOTO</h1>
-                    <input type="file"  placeholder='Email:'  name="email">
+                    <img src="<?php if(!empty($users->image)) { echo "user_picture/$users->image";} else { echo "images/user.png"; } ?>" alt="img" style="width:150px;height:150px">
+                    <input type="file" name="fichier">
+                    <input type='submit' placeholder='SUBMIT' name="boutonimg"/>
                 </div>
-            </div>
-        </form><?php
+            </form>
+        </div>
+        <?php
         
+    }
+
+    // changement de photo
+    if(isset($_POST["boutonimg"])) {
+        if (basename($_FILES["fichier"]["name"]) != NULL) {
+            $target_dir = "user_picture/";
+            $target_file = $target_dir . basename($_FILES["fichier"]["name"]);
+            $target_file =  substr($target_file, 0, -4) . 1 . substr($target_file, -4);
+            $uploaderror = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            $check = getimagesize($_FILES["fichier"]["tmp_name"]);
+            if($check !== false) {
+                $uploaderror = 1;
+            } else {
+                $uploaderror = 0;
+            }
+            // si le fichier existe déjà on ajoute + 1
+            if (file_exists($target_file)) {
+                while (file_exists($target_file)) {
+                    $lastchar = $target_file[strlen($target_file)-5];
+                    $numb = (int)$lastchar + 1;
+                    $target_file = substr($target_file, 0, -5) . "$numb" . substr($target_file, -4);
+                }
+            }
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "gif" ) {
+                $uploaderror = 0;
+            }
+
+            // Si y'a une erreur
+            if ($uploaderror == 0) {
+                $erreur2 = "Votre fichier n'est pas valide";
+            } else {
+                if (move_uploaded_file($_FILES["fichier"]["tmp_name"], $target_file)) {
+                    $confirm2 = "Le fichier ". htmlspecialchars( basename( $_FILES["fichier"]["name"])). " a été mis à jour";
+                    $target_file = substr($target_file, 12, (strlen($target_file)-1));
+                    $requp = "UPDATE users SET image = '$target_file' WHERE id = $_SESSION[id]";
+                    $OResup = $Bdd->query($requp);
+                    header("Refresh:0; url=index.php?p=compte");
+                } else {
+                    $erreur2 = "Une erreur s'est produite...";
+                }
+            }
+        } else { // si l'utilisateur n'as pas mis d'image, on la reset
+            $confirm2 = "Votre photo a bien été réinitialisé";
+            $requp = "UPDATE users SET image = '' WHERE id = $_SESSION[id]";
+            $OResup = $Bdd->query($requp);
+            header("Refresh:0; url=index.php?p=compte");
+        }
     }
 ?>
 
