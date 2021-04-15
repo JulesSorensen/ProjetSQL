e4f624eb0cd306210e072fa1a222c2b748eaa37a
Yan <souetre.yan@gmail.com>
2021-04-15 16:07:32 +0200

    back tasks list accueil

--- accueil.php
+++ accueil.php
@@ -1,4 +1,9 @@
 <link rel="stylesheet" href="css/accueil.css">
+
+<?php
+
+
+?>
 <div class="row d-flex justify-content-center container">
     <div class="col-md-8">
         <div class="card-hover-shadow-2x mb-3 card">
@@ -37,36 +42,45 @@
                     </div>
                 </perfect-scrollbar>
             </div>
-            <div class="d-block text-right card-footer"><button class="mr-2 btn btn-link btn-sm">Cancel</button><button class="btn btn-primary">Add Task</button></div>
+            <div class="d-block text-right card-footer"><input class="btn btn-primary" type="submit" value="Ajouter"></div>
         </div>
     </div>
 </div>
 
+<?php
+$req = "SELECT * FROM reminds WHERE usersid = '$_SESSION[id]'";
+$ORes = $Bdd->query($req);   ?>
+
 <div class="row d-flex justify-content-center container">
-    <div class="col-md-8">
-        <div class="card-hover-shadow-2x mb-3 card">
-            <div class="card-header-tab card-header">
-                <div class="card-header-title font-size-lg text-capitalize font-weight-normal"><i class="fa fa-tasks"></i>&nbsp;To-do List</div>
-            </div>
-            <div class="scroll-area-sm">
-                <perfect-scrollbar class="ps-show-limits">
-                    <div style="position: static;" class="ps ps--active-y">
-                        <div class="ps-content">
+<div class="col-md-8">
+    <div class="card-hover-shadow-2x mb-3 card">
+        <div class="card-header-tab card-header">
+            <div class="card-header-title font-size-lg text-capitalize font-weight-normal"><i class="fa fa-tasks"></i>&nbsp;To-do List</div>
+        </div>
+        <div class="scroll-area-sm">
+            <perfect-scrollbar class="ps-show-limits">
+                <div style="position: static;" class="ps ps--active-y">
+                    <div class="ps-content">
+<?php
+while ($reminds = $ORes->fetch()){?>
                             <ul class=" list-group list-group-flush">
                                 <li class="list-group-item">
                                     <div class="todo-indicator bg-warning"></div>
                                     <div class="widget-content p-0">
                                         <div class="widget-content-wrapper">
                                             <div class="widget-content-left">
-                                                <div class="widget-heading">Call Sam For payments <div class="badge badge-info ml-2">NOW</div>
+                                                <div class="widget-heading"><?php if(!empty($reminds->description)){ echo($reminds->description);} ?><div class="badge badge-info ml-2">NOW</div>
                                                 </div>
-                                                <div class="widget-subheading"><i>HEURE</i></div>
+                                                <div class="widget-subheading"><i><?php if(!empty($reminds->date)){ echo($reminds->date);} ?></i></div>
                                             </div>
-                                            <div class="widget-content-right"> <button class="border-0 btn-transition btn btn-outline-danger"> <i class="fa fa-trash"></i> </button> </div>
+                                            <div class="widget-content-right"> <button class="border-0 btn-transition btn btn-outline-danger"> <i class="fa fa-trash"></i> </button> </div> <!--POUBELLE-->
                                         </div>
                                     </div>
                                 </li>
                             </ul>
+                         <?php
+}
+?>
                         </div>
                     </div>
                 </perfect-scrollbar>
@@ -74,5 +88,3 @@
         </div>
     </div>
 </div>
-
-
