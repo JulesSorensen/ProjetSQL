675f808f900973b5525538bc09a5ec788be5f1bd
Yan <souetre.yan@gmail.com>
2021-04-15 17:05:18 +0200

    mon compte

--- moncompte.php
+++ moncompte.php
@@ -1,12 +1,37 @@
+<link rel="stylesheet" href="css/moncompte.css">
 <?php
-    // $Bdd->query("SELECT * FROM users")
+    $ORes = $Bdd->query("SELECT * FROM users WHERE id = '$_SESSION[id]'");
+    if ($users = $ORes->fetch()){ 
+        ?>
+        <form action="" method="POST">
+            <div id='container'>
+            <div class='signup'>
+                    <input type='text' placeholder='<?php echo $users->prenom; ?>'  name="prenom"/>
+                    <?php
+                    if(empty($_POST["prenom"]) == FALSE) {
+                        $nom = $_POST["prenom"];
+                        $req2 = "UPDATE users SET prenom = '$nom' WHERE id = '$_SESSION[id]'";
+                        $ORes2 = $Bdd->query($req2);
+                        echo "Le prenom a bien été changé<br/>";
+                    }?>
+                    <input type='text' placeholder='<?php echo $users->nom; ?>'  name="nom"/>
+                    <?php
+                    if(empty($_POST["nom"]) == FALSE) {
+                        $nom = $_POST["nom"];
+                        $req2 = "UPDATE users SET nom = '$nom' WHERE id = '$_SESSION[id]'";
+                        $ORes2 = $Bdd->query($req2);
+                        echo "Le nom a bien été changé<br/>";
+                    }?>
+                    <input type='text' placeholder='<?php echo $users->email; ?>'  name="email"/>
+                    <input type='submit' placeholder='SUBMIT' name="bouton"/>
+            </div>
+                <div class='whysign'>
+                    <h1>PHOTO</h1>
+                    <input type="file"  placeholder='Email:'  name="email">
+                </div>
+            </div>
+        </form><?php
+        
+    }
 ?>
 
-<form action="" method="POST">
-    <input type="text" name="">
-    <input type="text" name="">
-    <input type="email" name="">
-    <input type="text" name="">
-    <input type="text" name="">
-    <input type="text" name="">
-</form>
