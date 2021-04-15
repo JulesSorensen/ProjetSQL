f365deb8d39e5dd8c5cc4dce0459c6bdfb3bc57e
Yan <souetre.yan@gmail.com>
2021-04-15 14:19:03 +0200

    login sign up

--- connexion.php
+++ connexion.php
@@ -1,1 +1,16 @@
-CONNEXION
+
+<link rel="stylesheet" href="css/connexion.css">
+
+<div id='container'>
+  <div class='signup'>
+     <form>
+       <input type='text' placeholder='Email:'  />
+       <input type='text' placeholder='Mot de Passe:'  />
+       <input type='submit' placeholder='SUBMIT' />
+     </form>
+  </div>
+  <div class='whysign'>
+    <h1>Libérez votre espace mental</h1>
+    <p>Ayez l'esprit tranquille en ajoutant toutes vos tâches à votre to-do list (où que vous soyez ou quel que soit l'appareil utilisé).</p>
+  </div>
+</div>
--- inscription.php
+++ inscription.php
@@ -1,1 +1,17 @@
-INSCRIPTION
+<link rel="stylesheet" href="css/inscription.css">
+
+<div id='container'>
+  <div class='signup'>
+     <form>
+       <input type='text' placeholder='Prenom:'  />
+       <input type='text' placeholder='Nom:'  />
+       <input type='text' placeholder='Email:'  />
+       <input type='text' placeholder='Mot de Passe:'  />
+       <input type='submit' placeholder='SUBMIT' />
+     </form>
+  </div>
+  <div class='whysign'>
+    <h1>Libérez votre espace mental</h1>
+    <p>Ayez l'esprit tranquille en ajoutant toutes vos tâches à votre to-do list (où que vous soyez ou quel que soit l'appareil utilisé).</p>
+  </div>
+</div>
